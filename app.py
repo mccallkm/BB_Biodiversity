3c944f975cc6e145a4868664bc6d73b5d5e328b1
Keisha McCallum <keishamccallum@gmail.com>
2018-12-09 18:21:54 -0600

    update 7

--- app.py
+++ app.py
@@ -19,7 +19,8 @@ app = Flask(__name__)
 # Database Setup
 #################################################
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db/bellybutton.sqlite"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '') or "sqlite:///db/bellybutton.sqlite"
+# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db/bellybutton.sqlite"
 db = SQLAlchemy(app)
 
 # Reflect an existing database into a new model
